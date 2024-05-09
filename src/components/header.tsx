c38d4a48ff0165ad921fe06b5cb7ac4bcc5aed5e
Renan <renanrapace13@gmail.com>
2024-05-09 17:00:27 -0300

    Configuring tailwind css and some other files

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export function Header() {
+  return (
+    <View className="mt-10 p-8 justify-center items-center">
+      <Text className="font-semibold text-[#2791b5] text-3xl fixed">
+        Controle Financeiro
+      </Text>
+    </View>
+  );
+}
