512fe3e28c5843b0d7d27b791bae525f3aee42a9
Renan <renanrapace13@gmail.com>
2024-05-10 11:24:20 -0300

    Creating and adding the component (value)

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
 import { Label } from "@/components/label";
+import { Values } from "@/components/values";
 import { Header } from "./src/components/header";
+import { Separator } from "@/components/separator";
 
 export default function App() {
   return (
@@ -13,12 +15,27 @@ export default function App() {
       <StatusBar style="auto" />
 
       <Header />
-      <View className="">
+
+      <View className="gap-4 mb-6 ">
         <Label
           label="Entrada"
           rightIcon={{ name: "arrow-up-circle-outline" }}
-        />            
+        />
+        <Values values={120} />
       </View>
+      <View className="gap-4 mb-6">
+        <Label
+          label="Saída"
+          rightIcon={{ name: "arrow-down-circle-outline" }}
+        />
+        <Values values={120} />
+      </View>
+      <View className="gap-4 mb-6">
+        <Label label="Total" rightIcon={{ name: "wallet-outline" }} />
+        <Values values={120} />
+      </View>
+
+      <Separator/>
     </View>
   );
 }
---
+++ src/components/values.tsx
@@ -0,0 +1,34 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet } from "nativewind";
+import { Text, View } from "react-native";
+
+interface IValuesProps {
+  values: number;
+}
+
+export function Values({ values }: IValuesProps) {
+  return (
+    <LinearGradient style={styles.container} colors={["#2b93b4", "#56aaa6"]}>
+      <View className="w-full h-20 px-5 justify-between items-center rounded-xl flex-row">
+        <Text className=" text-5xl justify-between gap-20 text-white">R$</Text>
+        <Text className="text-5xl justify-between gap-20 text-white">120,00</Text>
+      </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+});
