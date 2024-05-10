d5e6e0a56ba2f98904a34796f715e013db193d0a
Renan <renanrapace13@gmail.com>
2024-05-10 11:25:33 -0300

    Adding expo-linear-gradient to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}", "./App.tsx"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#2b93b4",
-      secondary: "#56aaa6",
-      variant01: "#2791b5",
-      variant02: "#5f78b9",
+    extend: {
+      colors: {
+        primary: "#2b93b4",
+        secondary: "#56aaa6",
+        variant01: "#2791b5",
+        variant02: "#5f78b9",
+        white: "#fff",
+        black: "#000",
+      },
     },
   },
   plugins: [],
